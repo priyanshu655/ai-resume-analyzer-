36f91be6dd844143da87701b6a8362b164d74410
patel priyanshu <patelpriyanshu1209@gmail.com>
2026-02-22 18:28:04 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ connectDB();
 
 //middleware-1
 app.use(cors({
-  origin: "https://ai-resume-analyzer-wheat-rho.vercel.app/"
+  origin: "https://ai-resume-analyzer-wheat-rho.vercel.app"
 }));
 //middleware-2
 app.use(express.json());
